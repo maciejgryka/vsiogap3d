3983c9618aed6aeae04321367527b35777a7a868
maciej.gryka <maciej.gryka@03892b8c-e93e-11dd-b2d9-215335d0b316>
2009-05-14 20:37:34 +0000

    final commit
    git-svn-id: https://vsiogap3d.googlecode.com/svn/trunk@40 03892b8c-e93e-11dd-b2d9-215335d0b316

--- vsiogap3d/src/com/maciejgryka/fyproject/Indicators.java
+++ vsiogap3d/src/com/maciejgryka/fyproject/Indicators.java
@@ -1,10 +1,5 @@
 package com.maciejgryka.fyproject;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import javax.microedition.khronos.opengles.GL10;
